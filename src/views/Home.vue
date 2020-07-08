f65300c0e6b80f2af8f7cb894da0a654634a3473
Rebecca Cheung <rche0004@student.monash.edu>
2020-07-08 17:13:34 +1000

    testing of api request

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click=login()>Login</button>
+    {{ info }}
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import axios from 'axios';
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      info: null,
+    };
+  },
+  methods: {
+    login() {
+      const token = process.env.VUE_APP_TOKEN;
+
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      axios.get(`${process.env.VUE_APP_API_BASE_URL}accounts`,
+        config)
+        .then((response) => {
+          this.info = response.data;
+        });
+    },
   },
 };
 </script>
