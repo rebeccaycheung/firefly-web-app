72c7c1789051475c374797c485c61534289472b6
Rebecca Cheung <rebeccaycheung@gmail.com>
2020-07-13 15:07:57 +1000

    add donut chart of spending

---
+++ src/components/Spending.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="spending">
+      <apexchart width="380" type="donut" :options="options" :series="series"></apexchart>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Spending extends Vue {
+    @Prop() private options!: Record<string, string>;
+
+    @Prop() private series!: Array<Int32List>;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.spending {
+    background-color: #44475A;
+    padding: 2rem;
+    border-radius: 8px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="home">
-    <Heading title="Dashboard" />
-    <Dashboard :balance="balance" :spending="spending" :bills="bills" :netWorth="netWorth"/>
+    <div class="heading">
+      <Heading title="Dashboard" />
+    </div>
+    <div class="dashboard">
+      <Dashboard :balance="balance" :spending="spending" :bills="bills" :netWorth="netWorth"/>
+    </div>
+    <div class="spending">
+      <Spending :options="options" :series="series" />
+    </div>
   </div>
 </template>
 
@@ -10,12 +17,14 @@
 import axios from 'axios';
 import Heading from '@/components/Heading.vue';
 import Dashboard from '@/components/Dashboard.vue';
+import Spending from '@/components/Spending.vue';
 
 export default {
   name: 'Home',
   components: {
     Heading,
     Dashboard,
+    Spending,
   },
   data() {
     return {
@@ -23,6 +32,45 @@ export default {
       spending: 0,
       bills: 0,
       netWorth: 0,
+      options: {
+        labels: [],
+        dataLabels: {
+          style: {
+            colors: ['#FFFFFF'],
+          },
+        },
+        legend: {
+          labels: {
+            colors: '#FFFFFF',
+          },
+          markers: {
+            radius: 3,
+          },
+        },
+        stroke: {
+          show: false,
+        },
+        plotOptions: {
+          pie: {
+            donut: {
+              size: '60%',
+              labels: {
+                show: true,
+                total: {
+                  show: true,
+                  showAlways: true,
+                  color: '#FFFFFF',
+                },
+                value: {
+                  show: true,
+                  color: '#FFFFFF',
+                },
+              },
+            },
+          },
+        },
+      },
+      series: [],
     };
   },
   methods: {
@@ -61,9 +109,55 @@ export default {
           this.spending = spending - this.bills;
         });
     },
+    fetchTransactionsByCategories() {
+      const config = this.authorise();
+
+      const params = {
+        start: '2020-07-01',
+        end: '2020-07-31',
+      };
+
+      config.params = params;
+
+      axios.get(
+        `${process.env.VUE_APP_API_BASE_URL}chart/category/overview`,
+        config,
+      )
+        .then((response) => {
+          Object.keys(response.data).forEach((value) => {
+            if ((response.data[value].label).includes('Spent')) {
+              console.log(response.data[value]);
+              Object.keys(response.data[value].entries).forEach((key) => {
+                const amount = Number(response.data[value].entries[key]);
+                if (amount > 0) {
+                  this.series.push(amount);
+                  this.options.labels.push(key);
+                }
+              });
+            }
+          });
+        });
+    },
   },
   beforeMount() {
     this.fetchSummary();
+    this.fetchTransactionsByCategories();
   },
 };
 </script>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.heading {
+  flex: 0 0 100%;
+}
+
+.dashboard {
+  flex: 0 0 100%;
+  margin-bottom: 1rem;
+}
+</style>
